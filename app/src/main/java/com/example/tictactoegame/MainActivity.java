da34d877dc2aa7e08f00b38592b83ad85f162fe7
Igor <Zigor2356@mail.ru>
2023-01-04 12:41:25 +0300

    code cleaned

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -7,20 +7,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button2, button3, button4, button5, button6, button7, button8, button9, restartButton;
     TextView textView;
-    String[][] arrayTable = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
-    String cross, circle;
+    String[][] playingField = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+    String cross = "X";
+    String circle = "O";
     int index1, index2;
     boolean gameIsOn = true;
 
@@ -39,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
         button9 = findViewById(R.id.button9);
         restartButton = findViewById(R.id.restartButton);
         textView = findViewById(R.id.textView);
-        cross = "X";
-        circle = "O";
+        setRandomButtonsBackgroundColor();
+    }
+
+    public void setRandomButtonsBackgroundColor() {
         Random rnd = new Random();
         int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
         Button[] allButtons = {button1, button2, button3,
@@ -51,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void touchButton1(View v){
-        arrayTable[0][0] = cross;
+        playingField[0][0] = cross;
         button1.setText(cross);
         button1.setEnabled(false);
 
@@ -59,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton2(View v){
-        arrayTable[0][1] = cross;
+        playingField[0][1] = cross;
         button2.setText(cross);
         button2.setEnabled(false);
 
@@ -67,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton3(View v){
-        arrayTable[0][2] = cross;
+        playingField[0][2] = cross;
         button3.setText(cross);
         button3.setEnabled(false);
 
@@ -75,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton4(View v){
-        arrayTable[1][0] = cross;
+        playingField[1][0] = cross;
         button4.setText(cross);
         button4.setEnabled(false);
 
@@ -83,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton5(View v){
-        arrayTable[1][1] = cross;
+        playingField[1][1] = cross;
         button5.setText(cross);
         button5.setEnabled(false);
 
@@ -91,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton6(View v){
-        arrayTable[1][2] = cross;
+        playingField[1][2] = cross;
         button6.setText(cross);
         button6.setEnabled(false);
 
@@ -99,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton7(View v){
-        arrayTable[2][0] = cross;
+        playingField[2][0] = cross;
         button7.setText(cross);
         button7.setEnabled(false);
 
@@ -107,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton8(View v){
-        arrayTable[2][1] = cross;
+        playingField[2][1] = cross;
         button8.setText(cross);
         button8.setEnabled(false);
 
@@ -115,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         computerMove();
     }
     public void touchButton9(View v){
-        arrayTable[2][2] = cross;
+        playingField[2][2] = cross;
         button9.setText(cross);
         button9.setEnabled(false);
 
@@ -130,13 +128,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void computerMove(){
         winnerCheck();
-        if (gameIsOn == true && !allButtonsTouched()) {
+        if (gameIsOn && !allButtonsTouched()) {
             randomCordinates();
-            while (arrayTable[index1][index2].equals("X") ||
-                   arrayTable[index1][index2].equals("O")) {
+            while (!playingField[index1][index2].equals("")) {
                 randomCordinates();
             }
-            arrayTable[index1][index2] = circle;
+            playingField[index1][index2] = circle;
             disableComputerButton();
             computerMoveView();
             winnerCheck();
@@ -144,167 +141,102 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void computerMoveView(){
-        button1.setText(arrayTable[0][0]);
-        button2.setText(arrayTable[0][1]);
-        button3.setText(arrayTable[0][2]);
-        button4.setText(arrayTable[1][0]);
-        button5.setText(arrayTable[1][1]);
-        button6.setText(arrayTable[1][2]);
-        button7.setText(arrayTable[2][0]);
-        button8.setText(arrayTable[2][1]);
-        button9.setText(arrayTable[2][2]);
+        button1.setText(playingField[0][0]);
+        button2.setText(playingField[0][1]);
+        button3.setText(playingField[0][2]);
+        button4.setText(playingField[1][0]);
+        button5.setText(playingField[1][1]);
+        button6.setText(playingField[1][2]);
+        button7.setText(playingField[2][0]);
+        button8.setText(playingField[2][1]);
+        button9.setText(playingField[2][2]);
     }
-    
+
     public void winnerCheck(){
-        if (button1.getText().equals("X") &&
-            button2.getText().equals("X") &&
-            button3.getText().equals("X")){
-
-            disableAllButtons();
-            setButtonsGreen(button1, button2, button3);
-            textView.setText("you win");
-            gameIsOn = false;
+        if (buttonsTextEqualsX(button1, button2, button3)){
+            setWinningScenario(button1, button2, button3);
         }
-        else if (button4.getText().equals("X") &&
-                 button5.getText().equals("X") &&
-                 button6.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button4, button5, button6);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button4, button5, button6)) {
+            setWinningScenario(button4, button5, button6);
         }
-        else if (button7.getText().equals("X") &&
-                 button8.getText().equals("X") &&
-                 button9.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button7, button8, button9);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button7, button8, button9)) {
+            setWinningScenario(button7, button8, button9);
         }
-
-        else if (button1.getText().equals("X") &&
-                 button4.getText().equals("X") &&
-                 button7.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button1, button4, button7);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button1, button4, button7)) {
+            setWinningScenario(button1, button4, button7);
         }
-        else if (button2.getText().equals("X") &&
-                 button5.getText().equals("X") &&
-                 button8.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button2, button5, button8);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button2, button5, button8)) {
+            setWinningScenario(button2, button5, button8);
         }
-        else if (button3.getText().equals("X") &&
-                 button6.getText().equals("X") &&
-                 button9.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button3, button6, button9);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button3, button6, button9)) {
+            setWinningScenario(button3, button6, button9);
         }
-
-        else if (button1.getText().equals("X") &&
-                 button5.getText().equals("X") &&
-                 button9.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button1, button5, button9);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button1, button5, button9)) {
+            setWinningScenario(button1, button5, button9);
         }
-        else if (button3.getText().equals("X") &&
-                 button5.getText().equals("X") &&
-                 button7.getText().equals("X")) {
-
-            disableAllButtons();
-            setButtonsGreen(button3, button5, button7);
-            textView.setText("you win");
-            gameIsOn = false;
+        else if (buttonsTextEqualsX(button3, button5, button7)) {
+            setWinningScenario(button3, button5, button7);
         }
-
 
         else if (button1.getText().equals("O") &&
                 button2.getText().equals("O") &&
                 button3.getText().equals("O")){
-
-            disableAllButtons();
-            setButtonsRed(button1, button2, button3);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button1, button2, button3);
         }
         else if (button4.getText().equals("O") &&
                 button5.getText().equals("O") &&
                 button6.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button4, button5, button6);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button4, button5, button6);
         }
         else if (button7.getText().equals("O") &&
                 button8.getText().equals("O") &&
                 button9.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button7, button8, button9);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button7, button8, button9);
         }
-
         else if (button1.getText().equals("O") &&
                 button4.getText().equals("O") &&
                 button7.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button1, button4, button7);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button1, button4, button7);
         }
         else if (button2.getText().equals("O") &&
                 button5.getText().equals("O") &&
                 button8.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button2, button5, button8);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button2, button5, button8);
         }
         else if (button3.getText().equals("O") &&
                 button6.getText().equals("O") &&
                 button9.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button3, button6, button9);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button3, button6, button9);
         }
-
         else if (button1.getText().equals("O") &&
                 button5.getText().equals("O") &&
                 button9.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button1, button5, button9);
-            textView.setText("game over");
-            gameIsOn = false;
+            setLosingScenario(button1, button5, button9);
         }
         else if (button3.getText().equals("O") &&
                 button5.getText().equals("O") &&
                 button7.getText().equals("O")) {
-
-            disableAllButtons();
-            setButtonsRed(button3, button5, button7);
-            gameIsOn = false;
+            setLosingScenario(button3, button5, button7);
         }
+    }
+
+    public boolean buttonsTextEqualsX(Button btn1, Button btn2, Button btn3) {
+        return button1.getText().equals("X") && button2.getText().equals("X") &&
+                button3.getText().equals("X");
+    }
+
+    public void setWinningScenario(Button btn1, Button btn2, Button btn3) {
+        disableAllButtons();
+        setButtonsGreen(btn1, btn2, btn3);
+        textView.setText("you win");
+        gameIsOn = false;
+    }
+
+    public void setLosingScenario(Button btn1, Button btn2, Button btn3) {
+        disableAllButtons();
+        setButtonsRed(btn1, btn2, btn3);
+        textView.setText("game over");
+        gameIsOn = false;
     }
 
     public void disableComputerButton(){
@@ -350,9 +282,9 @@ public class MainActivity extends AppCompatActivity {
             button.setBackgroundColor(color);
         }
 
-        for (int i = 0; i < arrayTable.length; i++){
-            for (int j = 0; j < arrayTable[i].length; j++){
-                arrayTable[i][j] = "";
+        for (int i = 0; i < playingField.length; i++){
+            for (int j = 0; j < playingField[i].length; j++){
+                playingField[i][j] = "";
             }
         }
         textView.setText("");
@@ -360,17 +292,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean allButtonsTouched(){
-        if (arrayTable[0][0] != "" &&
-            arrayTable[0][1]  != "" &&
-            arrayTable[0][2] != "" &&
+        if (playingField[0][0] != "" &&
+            playingField[0][1]  != "" &&
+            playingField[0][2] != "" &&
 
-            arrayTable[1][0] != "" &&
-            arrayTable[1][1] != "" &&
-            arrayTable[1][2] != "" &&
+            playingField[1][0] != "" &&
+            playingField[1][1] != "" &&
+            playingField[1][2] != "" &&
 
-            arrayTable[2][0] != "" &&
-            arrayTable[2][1] != "" &&
-            arrayTable[2][2] != "" ) {
+            playingField[2][0] != "" &&
+            playingField[2][1] != "" &&
+            playingField[2][2] != "" ) {
             return true;
         } else {
             return false;
@@ -398,6 +330,8 @@ public class MainActivity extends AppCompatActivity {
         button3.setBackgroundColor(Color.RED);
     }
 }
+
+
+
 //TODO: задержка между ходами.
 // два режима игры
-// оптимизация
